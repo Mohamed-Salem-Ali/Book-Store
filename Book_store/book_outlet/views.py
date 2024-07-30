0a838f356942f618b85160b9b0e964743ebffef6
Mohamed-Salem-Ali <65522205+Mohamed-Salem-Ali@users.noreply.github.com>
2024-07-30 21:38:39 +0300

    Updates
    1- edit urls.py adding new url /book/id
    2- edit views.py by adding new book_details function
    3- edit setting.py by adding static paths
    4- edit pages
    5- adding styles.css file

--- Book_store/book_outlet/views.py
+++ Book_store/book_outlet/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 
+from .models import Book
 # Create your views here.
+
+def index(request):
+    books = Book.objects.all()
+    return render(request,"index.html",{
+        "books":books
+    })
+def book_detail(request,id):
+    book = get_object_or_404(Book,pk=id)
+    return render(request,"book_detail.html",{
+        "title":book.title,
+        "author":book.author,
+        "rate":book.rate,
+        "is_bestselling": book.is_bestselling
+    })
